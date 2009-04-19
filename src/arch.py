e224343b88c758c8481cfc1ece33b874f74886d0
tuxce <tuxce.net@gmail.com>
2009-04-19 22:31:57 +0200

    * modification du schéma de la base:   * spécifications des paramètres à passer à une url   * précision de la méthode (post/get)   * nom du paramètre devant contenir les termes de recherche   * création d'une vue pour améliorer la recherche du paquet contenant un fichier * modification de WebQuery pour prendre en compte la conf depuis la base * ajout de ArchPackage   * Synchronisation partielle des paquets (sans tout réécrire à chaque fois)   * Recherche plus rapide grâce notamment à la vue repo_pkg_file

--- src/arch.py
+++ src/arch.py
@@ -3,8 +3,11 @@
 
 
 import sqlite3
-import tarfile, tempfile, urllib
-import re, random
+import urllib2, urllib
+import tarfile, tempfile
+import re
+#import random
+
 
 # Classe ArchDB: Gestion du fichier de données
 class ArchDB:
@@ -38,7 +41,7 @@ class ArchDB:
 			print "Erreur lors de la connexion à :", self._file, e.args[0]
 			exit (1)
 
-	def setCommit (val=True):
+	def setCommit (self, val=True):
 		ArchDB._commit = val
 
 	# Committe les modifications
@@ -50,13 +53,14 @@ class ArchDB:
 
 	# Execute une requête
 	def execute(self, req, args, commit=False):
+		#print req, args, commit, ArchDB._commit
 		try:
 			if not args is None:
 				self.cur.execute (req, tuple (args))
 			else:
 				self.cur.execute (req)
-				if commit and self._commit:
-					self.commit ()
+			if commit and self._commit:
+				self.commit ()
 			return True
 		except sqlite3.Error, e:
 			print "Erreur lors de l'éxecution de: ", req
@@ -92,7 +96,7 @@ class ArchDB:
 
 
 # Classe WebQuery: Gestion des recherches sur le web
-class WebQuery
+class WebQuery:
 
 	def __init__(self):
 		self.db = ArchDB ()
@@ -102,8 +106,8 @@ class WebQuery
 		del self.db
 
 	def _req (self, table, mode='select'):
-		arg = {'site': ['site', 'url'],
-				'query': ['query', 'query'],
+		arg = {'site': ['site', 'code'],
+				'query': ['query', 'content'],
 				'page': ['page', 'url']}[table]
 		req = {'select': "select id from " + arg[0] + " where " + arg[1] + " = ?",
 				'insert': "insert into " + arg[0] + " (" + arg[1] + ") values (?)",
@@ -128,19 +132,80 @@ class WebQuery
 
 
 	def loadSites (self):
+		# sites:
+		#	0 id
+		#	1 url
+		#	2 slink
+		#	3 regexp (compilée)
+		#	4 method_post (0: non, !=0: oui)
+		#	5 sparam
+		#	6 params (ensemble des param/valeur)
 		self.sites = {}
-		req = "select id, url, slink, regexp from site"
-		results = self.db.getAll (req)
-		for result in results:
-			sites[result[1]] = [ result[2], result[2] ]
+		req = "select id, code, url, slink, regexp, method_post, sparam from site"
+
+		try:
+			r_sites = self.db.getAll (req)
+			for r_site in r_sites:
+				req = "select param_name, param_value from site_param where site_id = ?"
+				r_params = self.db.getAll (req, [r_site[0]])
+				params = {}
+				for r_param in r_params:
+					params[r_param[0]] = r_param[1]
+				regexp_c = re.compile (r_site[4])
+				self.sites[r_site[1]] = [ r_site[0], r_site[2],
+						r_site[3], regexp_c,
+						r_site[5], r_site[6], params]
+		except:
+			None
+			
+
+	def getSite (self, code):
+		try:
+			ret = self.sites[code][0]
+		except:
+			ret = None
+		return ret
+
+	def setPage (self, site, query, page):
+		site_id = self.getSite (site)
+		query_id = self._set ('query', query)
+		page_id = self._set ('page', page)
+		req = """insert into result (site_id, query_id, page_id)
+		values (?, ?, ?)"""
+		return self.db.executeCmd (req, [site_id, query_id, page_id])
+
+	def searchPages (self, site, query):
+		try:
+			site_id = self.sites[site][0]
+			url_base = self.sites[site][1]
+			url_search = self.sites[site][2]
+			p_search = self.sites[site][3]
+			params = self.sites[site][6]
+			params[self.sites[site][5]] =  query
+			vars = urllib.urlencode (params)
+			if self.sites[site][4] != 0:
+				fd = urllib2.urlopen (url_base + url_search, vars)
+			else:
+				fd = urllib2.urlopen (url_base + url_search + '?' + vars)
+			#print url_base + url_search + '?' + vars
+			if fd is None:
+				return None
+			pages = p_search.findall (fd.read())
+			fd.close ()
+			for page in pages:
+				self.setPage (site, query, page)
+			return pages
+		except:
+			return None
+
 
 	def getPages (self, site, query):
-		site_id = self._get ('site', site)
-		if site_id = None:
+		site_id = self.getSite (site)
+		if site_id is None:
 			return None
 		query_id = self._get ('query', query)
-		if query_id = None:
-			return self.searchPages (site, self._set ('query', query)
+		if query_id is None:
+			return self.searchPages (site, query)
 		req = """select url 
 		from result	join page on (result.page_id = page.id)
 		where site_id = ? and query_id = ?"""
@@ -150,36 +215,29 @@ class WebQuery
 		else:
 			return pages
 
-	def setPage (self, site, query, page):
-		site_id = self._set ('site', site)
-		query_id = self._set ('query', query)
-		page_id = self._set ('page', page)
-		req = """insert into result (site_id, query_id, page_id)
-		values (?, ?, ?)"""
-		return self.db.executeCmd (req, [site_id, query_id, page_id])
-
 	def getResult (self, site=False, query=False, page=False):
+		site_id = query_id = page_id = False
 		if site:
-			site_id = self._set ('site', site)
+			site_id = self.getSite (site)
 		if query:
-			query_id = self._set ('query', query)
+			query_id = self._get ('query', query)
 		if page:
-			page_id = self._set ('page', page)
+			page_id = self._get ('page', page)
 		req = "select id from result where true"
 		args = []
-		if site:
+		if site_id:
 			req += " and site_id = ? "
 			args += [site_id]
-		if query:
+		if query_id:
 			req += " and query_id = ? "
 			args += [query_id]
-		if page:
+		if page_id:
 			req += " and page_id = ? "
 			args += [page_id]
 
 		return self.db.getAll (req, args)
 
-	def delResult (self, site=False, query=False, page=false):
+	def delResult (self, site=False, query=False, page=False):
 		results = self.getResuls (site, query, page)
 		self.db.setCommit (False)
 		for result in results:
@@ -200,5 +258,103 @@ class WebQuery
 		self.db.commit ()
 		self.db.setCommit (True)
 
+class ArchPackage:
+	def __init__(self):
+		self.db = ArchDB ()
+	
+	def __del__(self):
+		del self.db
 
-			
+	def getPkg (self, repo_id, pkg, version=None):
+		req = "select id from pkg where repo_id = ? and name = ?"
+		args = [repo_id, pkg]
+		if version is not None:
+			req += " and version = ?"
+			args += [version]
+		pkg_id = self.db.getOne (req, args)
+		if pkg_id is not None:
+			return pkg_id
+		else:
+			return False
+		
+
+	def delPkg (self, repo_id, pkg, version=None):
+		pkg_id = self.getPkg (repo_id, pkg, version)
+		if pkg_id:
+			req = "delete from file where pkg_id = ?"
+			self.db.executeCmd (req, [pkg_id])
+			req = "delete from pkg where id = ?"
+			self.db.executeCmd (req, [pkg_id])
+		return True
+
+	def addPkg (self, repo_id, pkg, version):
+		pkg_id =  self.getPkg (repo_id, pkg, version)
+		if pkg_id:
+			return pkg_id
+		self.delPkg (repo_id, pkg)
+		req = "insert into pkg (repo_id, name, version) values (?, ?, ?)"
+		if self.db.executeCmd (req, [repo_id, pkg, version]):
+			return self.getPkg (repo_id, pkg, version)
+		else:
+			return False
+
+
+	def addFile (self, pkg_id, path):
+		req = "insert into file (pkg_id, path) values (?, ?)"
+		return self.db.executeCmd (req, [pkg_id, path])
+	
+	
+	def sync(self):
+		try:
+			self.db.setCommit (False)
+			req = "select id, name, url from repo"
+			repos = self.db.getAll (req)
+			for repo in repos:
+				print "Récupération de '", repo[1], "' depuis '", repo[2], "'"
+				tmp_file = tempfile.NamedTemporaryFile()
+				urllib.urlretrieve (repo[2] + '/' + repo[1] + '.files.tar.gz', tmp_file.name)
+				file_tgz = tarfile.open (tmp_file.name, "r")
+				if not file_tgz:
+					continue
+				for pkgfile in file_tgz.getnames():
+					if pkgfile[-5:] != 'files':
+						continue
+					str = pkgfile[:pkgfile.rfind ('/')]
+					pkgrel_index = pkgfile.rfind ('-')
+					pkgrel = str[pkgrel_index+1:]
+					str = str[:pkgrel_index]
+					pkgver_index = str.rfind ('-')
+					pkgver = str[pkgver_index+1:]
+					pkgname = str[:pkgver_index]
+					print pkgname, "(version:", pkgver, "release:", pkgrel, ")"
+					if self.getPkg (repo[0], pkgname, pkgver + '-' + pkgrel):
+						continue
+					pkg_id = self.addPkg (repo[0], pkgname, pkgver + '-' + pkgrel)
+					file_info = file_tgz.extractfile(pkgfile)
+					for path in file_info.readlines():
+						print path
+						if path[-1] == '\n':
+							self.addFile (pkg_id, path[:-1])
+						else:
+							self.addFile (pkg_id, path)
+				file_tgz.close()
+				tmp_file.close()
+			self.db.commit ()
+			self.db.setCommit (True)
+
+
+		except:
+			None
+
+	def searchPkg (self, search, max=5):
+		req = """select r.name, p.name, p.version 
+		from repo r join pkg p on (r.id = p.repo_id)
+		where p.name like ? limit ?"""
+		return self.db.getAll (req, ['%' + search + '%', max])
+
+	def searchFile (self, search, max=5):
+		req = """select repo_name, pkg_name, path
+		from repo_pkg_file where path like ? limit ?"""
+		return self.db.getAll (req, ['%' + search + '%', max])
+
+
--- tools/archdb.sql
+++ tools/archdb.sql
@@ -2,15 +2,45 @@
 CREATE TABLE repo (id integer primary key autoincrement, name text, url text);
 CREATE TABLE pkg (id integer primary key autoincrement, repo_id integer, name text, version text, desc text);
 CREATE TABLE file (id integer primary key autoincrement, pkg_id integer, path text);
+CREATE INDEX file_path ON file (path);
+CREATE VIEW repo_pkg_file as 
+  SELECT r.name as repo_name, p.name as pkg_name, p.version, f.path
+    FROM file f join pkg p on (p.id = f.pkg_id)
+	            join repo r on (r.id = p.repo_id)
+;
+
+insert into repo (name, url) values ('core', 'http://mir.archlinux.fr/core/os/i686');
+insert into repo (name, url) values ('extra', 'http://mir.archlinux.fr/extra/os/i686');
+insert into repo (name, url) values ('community', 'http://mir.archlinux.fr/community/os/i686');
+insert into repo (name, url) values ('archlinuxfr', 'http://repo.archlinux.fr/i686');
 
 
 /* Tables pour la gestion des recherches sur le web */
+CREATE TABLE site (id integer primary key autoincrement, code text,  url text, slink text, method_post integer, sparam text, regexp text);
+CREATE TABLE site_param (id integer primary key autoincrement, site_id integer, param_name text, param_value text);
+
 CREATE TABLE query (id integer primary key autoincrement, content text);
 CREATE TABLE page (id integer primary key autoincrement, url text);
-CREATE TABLE site (id integer primary key autoincrement, url text unique, slink text, regexp text);
 CREATE TABLE result (id integer primary key autoincrement, site_id integer, query_id integer, page_id integer );
 CREATE INDEX result_uniq ON result (site_id, query_id, page_id);
+
+insert into site (code, url, slink, method_post, sparam, regexp) values ('wiki_qsearch', 'http://wiki.archlinux.fr', '/lib/exe/ajax.php', 0, 'q', 'href="(.*?)\?');
+insert into site_param (site_id, param_name, param_value) values (1, 'call', 'qsearch');
+
+insert into site (code, url, slink, method_post, sparam, regexp) values ('wiki_search', 'http://wiki.archlinux.fr', '/', 0, 'id', 'href="(.*?)\?.*wikilink1');
+insert into site_param (site_id, param_name, param_value) values (2, 'do', 'search');
+
+insert into site (code, url, slink, method_post, sparam, regexp) values ('wiki_org', 'http://wiki.archlinux.org/index.php', '/Special:Search', 0, 'search', '<li>.*?href="(.*?)".*search');
+insert into site_param (site_id, param_name, param_value) values (3, 'fulltext', 'Search');
+
+insert into site (code, url, slink, method_post, sparam, regexp) values ('bugs_org', 'http://bugs.archlinux.org', '/', 0, 'string', 'task_id.*href="(http://bugs.archlinux.org/task/.*?)\?');
+insert into site_param (site_id, param_name, param_value) values (4, 'projet', '0');
+insert into site_param (site_id, param_name, param_value) values (4, 'search_for_all', '1');
+
 
 /* Tables pour la gestion des reponses du bot */
 CREATE TABLE reply_regexp (id integer primary key autoincrement, regexp text);
 CREATE TABLE reply (id integer primary key autoincrement, reply_regexp_id integer, content text);
+
+
+
