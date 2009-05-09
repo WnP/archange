4a56e432adafe129b46ccb92cd959fa68884db29
tuxce <tuxce.net@gmail.com>
2009-05-09 17:19:41 +0200

    maj tools/archdb.sql pour les paramètres scroogle etc...

--- tools/archdb.sql
+++ tools/archdb.sql
@@ -23,18 +23,21 @@ CREATE TABLE query (id integer primary key autoincrement, content text);
 CREATE TABLE page (id integer primary key autoincrement, url text);
 CREATE TABLE result (id integer primary key autoincrement, site_id integer, query_id integer, page_id integer );
 
-insert into site (code, url, slink, method_post, sparam, regexp) values ('wiki_qsearch', 'http://wiki.archlinux.fr', '/lib/exe/ajax.php', 0, 'q', 'href="(.*?)\?');
-insert into site_param (site_id, param_name, param_value) values (1, 'call', 'qsearch');
+INSERT INTO "site" VALUES(1,'wiki_qsearch','http://wiki.archlinux.fr','/lib/exe/ajax.php',0,'q','href="(.*?)\?');
+INSERT INTO "site" VALUES(2,'wiki_search','http://wiki.archlinux.fr','/',0,'id','href="(.*?)\?.*wikilink1');
+INSERT INTO "site" VALUES(3,'wiki_org','http://wiki.archlinux.org/index.php','/Special:Search',0,'search','<li>.*?href="/index.php(.*?)".*search');
+INSERT INTO "site" VALUES(4,'bugs_org','http://bugs.archlinux.org','/',0,'string','task_id.*href="http://bugs.archlinux.org(/task/.*?)\?');
+INSERT INTO "site" VALUES(5,'aur','http://aur.archlinux.org','/rpc.php',0,'arg','"Name":"(.*?)","Version":"(.*?)"');
+INSERT INTO "site" VALUES(6,'scroogle','','http://www.scroogle.org/cgi-bin/nbbw.cgi',1,'Gw','[0-9].*Href="(.*?)"');
 
-insert into site (code, url, slink, method_post, sparam, regexp) values ('wiki_search', 'http://wiki.archlinux.fr', '/', 0, 'id', 'href="(.*?)\?.*wikilink1');
-insert into site_param (site_id, param_name, param_value) values (2, 'do', 'search');
-
-insert into site (code, url, slink, method_post, sparam, regexp) values ('wiki_org', 'http://wiki.archlinux.org/index.php', '/Special:Search', 0, 'search', '<li>.*?href="/index.php(.*?)".*search');
-insert into site_param (site_id, param_name, param_value) values (3, 'fulltext', 'Search');
-
-insert into site (code, url, slink, method_post, sparam, regexp) values ('bugs_org', 'http://bugs.archlinux.org', '/', 0, 'string', 'task_id.*href="(http://bugs.archlinux.org/task/.*?)\?');
-insert into site_param (site_id, param_name, param_value) values (4, 'projet', '0');
-insert into site_param (site_id, param_name, param_value) values (4, 'search_for_all', '1');
+INSERT INTO "site_param" VALUES(1,1,'call','qsearch');
+INSERT INTO "site_param" VALUES(2,2,'do','search');
+INSERT INTO "site_param" VALUES(3,3,'fulltext','Search');
+INSERT INTO "site_param" VALUES(4,4,'projet','0');
+INSERT INTO "site_param" VALUES(5,4,'search_for_all','1');
+INSERT INTO "site_param" VALUES(6,5,'type','search');
+INSERT INTO "site_param" VALUES(8,6,'l','fr');
+INSERT INTO "site_param" VALUES(9,6,'n','2');
 
 
 /* Tables pour la gestion des reponses du bot */
